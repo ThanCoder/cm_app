39bb27566ffabe98b82dfe2b8a13d974c321e3b3
ThanCoder <thancoder2019@gmail.com>
2026-01-07 12:33:30 +0630

    update

--- lib/app/ui/components/cache_image.dart
+++ lib/app/ui/components/cache_image.dart
@@ -1,11 +1,10 @@
-import 'dart:io';
-import 'package:cm_app/app/core/app_setting/cache_image_setting_list_tile.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cm_app/more_libs/setting/core/path_util.dart';
 import 'package:flutter/material.dart';
 import 'package:t_widgets/t_widgets.dart';
 import 'package:than_pkg/than_pkg.dart';
 
-class CacheImage extends StatefulWidget {
+class CacheImage extends StatelessWidget {
   final String url;
   final VoidCallback? onTap;
   const CacheImage({super.key, required this.url, this.onTap});
@@ -15,66 +14,74 @@ class CacheImage extends StatefulWidget {
   );
 
   @override
-  State<CacheImage> createState() => _CacheImageState();
-}
-
-class _CacheImageState extends State<CacheImage> {
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onSecondaryTap: _deleteCache,
-      onLongPress: _deleteCache,
-      onTap: widget.onTap,
-      child: _getWidget(),
+    return CachedNetworkImage(
+      imageUrl: url,
+      fit: BoxFit.cover,
+      // placeholder: (context, url) => Text('placeholder'),
+      errorWidget: (context, url, error) => Text('error'),
+      progressIndicatorBuilder: (context, url, progress) => TLoader.random(),
     );
-  }
-
-  Widget _getWidget() {
-    if (CacheImageSettingListTile.isUseCacheImage) {
-      final cacheFile = File(
-        TWidgets.instance.getCachePath?.call(widget.url) ?? '',
-      );
-      if (cacheFile.existsSync()) {
-        return TImageFile(path: cacheFile.path, errorBuilder: _errorBuilder);
-      }
-      return FutureBuilder(
-        future: TWidgets.instance.onDownloadImage?.call(
-          widget.url,
-          cacheFile.path,
-        ),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return TLoader.random();
-          }
-          return TImageFile(path: cacheFile.path, errorBuilder: _errorBuilder);
-        },
-      );
-    }
-    return TImage(source: widget.url);
-  }
-
-  void _deleteCache() {
-    final file = File(CacheImage.getCachePath(widget.url));
-    if (!file.existsSync()) return;
-
-    showTConfirmDialog(
-      context,
-      contentText: 'Image Cache ကိုဖျက်ချင်တာ သေချာပြီလား?',
-      submitText: 'Delete Cache',
-      onSubmit: () async {
-        await file.delete();
-        await ThanPkg.appUtil.clearImageCache();
-        if (!mounted) return;
-        setState(() {});
-      },
-    );
-  }
-
-  Widget _errorBuilder(
-    BuildContext context,
-    Object error,
-    StackTrace? stackTrace,
-  ) {
-    return TImage(source: widget.url);
   }
 }
+
+// class _CacheImageState extends State<CacheImage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onSecondaryTap: _deleteCache,
+//       onLongPress: _deleteCache,
+//       onTap: widget.onTap,
+//       child: _getWidget(),
+//     );
+//   }
+
+//   Widget _getWidget() {
+//     if (CacheImageSettingListTile.isUseCacheImage) {
+//       final cacheFile = File(
+//         TWidgets.instance.getCachePath?.call(widget.url) ?? '',
+//       );
+//       if (cacheFile.existsSync()) {
+//         return TImageFile(path: cacheFile.path, errorBuilder: _errorBuilder);
+//       }
+//       return FutureBuilder(
+//         future: TWidgets.instance.onDownloadImage?.call(
+//           widget.url,
+//           cacheFile.path,
+//         ),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return TLoader.random();
+//           }
+//           return TImageFile(path: cacheFile.path, errorBuilder: _errorBuilder);
+//         },
+//       );
+//     }
+//     return TImage(source: widget.url);
+//   }
+
+//   void _deleteCache() {
+//     final file = File(CacheImage.getCachePath(widget.url));
+//     if (!file.existsSync()) return;
+
+//     showTConfirmDialog(
+//       context,
+//       contentText: 'Image Cache ကိုဖျက်ချင်တာ သေချာပြီလား?',
+//       submitText: 'Delete Cache',
+//       onSubmit: () async {
+//         await file.delete();
+//         await ThanPkg.appUtil.clearImageCache();
+//         if (!mounted) return;
+//         setState(() {});
+//       },
+//     );
+//   }
+
+//   Widget _errorBuilder(
+//     BuildContext context,
+//     Object error,
+//     StackTrace? stackTrace,
+//   ) {
+//     return TImage(source: widget.url);
+//   }
+// }
--- lib/app/ui/home/home_screen.dart
+++ lib/app/ui/home/home_screen.dart
@@ -1,33 +1,58 @@
+import 'dart:math';
+
 import 'package:cm_app/app/ui/home/home_page.dart';
 import 'package:cm_app/app/ui/home/library_page.dart';
 import 'package:cm_app/app/ui/home/more_app_page.dart';
+import 'package:cm_app/app/ui/screens/movie_bookmark_screen.dart';
+import 'package:cm_app/more_libs/setting/setting.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        body: TabBarView(children: [HomePage(), LibraryPage(), MoreAppPage()]),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(
-              // text: 'Home',
-              icon: Icon(Icons.home),
-            ),
-            Tab(
-              // text: 'Library',
-              icon: Icon(Icons.library_books),
-            ),
-            Tab(
-              // text: 'More',
-              icon: Icon(Icons.grid_view_rounded),
-            ),
-          ],
-        ),
+    return Scaffold(
+      body: IndexedStack(
+        index: index,
+        children: [
+          HomePage(),
+          LibraryPage(),
+          MovieBookmarkScreen(),
+          MoreAppPage(key: ValueKey(Random.secure().nextInt(100))),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Setting.getAppConfig.isDarkTheme
+            ? Colors.white
+            : Colors.black,
+        onTap: (value) {
+          index = value;
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+            label: 'Library',
+            icon: Icon(Icons.library_books),
+          ),
+          BottomNavigationBarItem(
+            label: 'BookMark',
+            icon: Icon(Icons.bookmark_added),
+          ),
+          BottomNavigationBarItem(
+            label: 'More',
+            icon: Icon(Icons.grid_view_rounded),
+          ),
+        ],
       ),
     );
   }
